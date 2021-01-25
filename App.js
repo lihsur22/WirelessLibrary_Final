a6e3d8326b1484d202f9c4d1c8d56f85acc8a6d3
lihsur22 <rushilganguli@outlook.com>
2021-01-25 19:49:09 +0530

    done

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 import TSscreen from './screens/booktsscreen';
 import SearchScreen from './screens/searchscreen';
 
 export default function App() {
   return (
-    <View>
-    </View>
+    <AppContainer/>
   );
 }
 
 const styles = StyleSheet.create({
 });
+
+const tabNavigator = createBottomTabNavigator({
+  A : {screen : TSscreen},
+  B : {screen : SearchScreen}
+})
+
+const AppContainer = createAppContainer(tabNavigator);
--- screens/booktsscreen.js
+++ screens/booktsscreen.js
@@ -13,6 +13,7 @@ export default class TSscreen extends React.Component{
 
 const styles = StyleSheet.create({
     container : {
+        flex : 1,
         justifyContent : 'center',
         alignItems : 'center',
     }
